c83fdce1545a5349a21ca741fa29f25149114210
Sacha Pernak <sacha.pernak@etu.iut-tlse3.fr>
2024-11-22 10:03:23 +0100

    Création de la page d'accueil
    Ajout de la bannière

---
+++ Partie php/Accueil.php
@@ -0,0 +1,22 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./CSS/Accueil-Style.css">
+    <title>Handi-Team Manager</title>
+</head>
+<body>
+    <header class="main-header">
+        <div class="left-div">
+            <div block class="title-container">
+                <h1>🏀 Handi-Team Manager Pro™</h1>
+            </div>
+        </div>
+    </header>
+
+</body>
+</html>
