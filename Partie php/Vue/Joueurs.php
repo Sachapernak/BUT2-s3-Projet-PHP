806e0406be92ccbcb959358c4a213d1115ba0d2f
Draux Erine <erine.draux@etu.iut-tlse3.fr>
2024-12-25 18:35:08 +0100

    Creation d'un fichier html et un css pour la barre de navigation a inclure dans nos pages + finalisation de la page joueurs

--- Partie php/Vue/Joueurs.php
+++ Partie php/Vue/Joueurs.php
@@ -2,57 +2,57 @@
 
 $joueursArray = array(
                 '<div class="joueur">
-                    <div>
-                        <h5>[Nom] [Prenom]</h5> 
-                        <h6> N° de licence : </h6>
-                        <h6> Statut : </h6>
-                        <span>★★★☆☆</span>
-                    </div>
-                </div>',
-                '<div class="joueur">
-                    <div>
-                        <h5>[Nom] [Prenom]</h5> 
-                        <h6> N° de licence : </h6>
-                        <h6> Statut : </h6>
-                        <span>★★★☆☆</span>
-                    </div>
-                </div>',
-                '<div class="joueur">
-                    <div>
-                        <h5>[Nom] [Prenom]</h5> 
-                        <h6> N° de licence : </h6>
-                        <h6> Statut : </h6>
-                        <span>★★★☆☆</span>
-                    </div>
-                </div>',
-                '<div class="joueur">
-                    <div>
-                        <h5>[Nom] [Prenom]</h5> 
-                        <h6> N° de licence : </h6>
-                        <h6> Statut : </h6>
-                        <span>★★★☆☆</span>
-                    </div>
-                </div>',
-                '<div class="joueur">
-                    <div>
-                        <h5>[Nom] [Prenom]</h5> 
-                        <h6> N° de licence : </h6>
-                        <h6> Statut : </h6>
-                        <span>★★★☆☆</span>
-                    </div>
-                </div>',
-                '<div class="joueur">
-                    <div>
-                        <h5>[Nom] [Prenom]</h5> 
-                        <h6> N° de licence : </h6>
-                        <h6> Statut : </h6>
-                        <span>★★★☆☆</span>
-                    </div>
-                </div>',
+<div>
+    <h5>[Nom] [Prenom]</h5>
+    <h6> N° de licence : </h6>
+    <h6> Statut : </h6>
+    <span>★★★☆☆</span>
+</div>
+</div>',
+'<div class="joueur">
+    <div>
+        <h5>[Nom] [Prenom]</h5>
+        <h6> N° de licence : </h6>
+        <h6> Statut : </h6>
+        <span>★★★☆☆</span>
+    </div>
+</div>',
+'<div class="joueur">
+    <div>
+        <h5>[Nom] [Prenom]</h5>
+        <h6> N° de licence : </h6>
+        <h6> Statut : </h6>
+        <span>★★★☆☆</span>
+    </div>
+</div>',
+'<div class="joueur">
+    <div>
+        <h5>[Nom] [Prenom]</h5>
+        <h6> N° de licence : </h6>
+        <h6> Statut : </h6>
+        <span>★★★☆☆</span>
+    </div>
+</div>',
+'<div class="joueur">
+    <div>
+        <h5>[Nom] [Prenom]</h5>
+        <h6> N° de licence : </h6>
+        <h6> Statut : </h6>
+        <span>★★★☆☆</span>
+    </div>
+</div>',
+'<div class="joueur">
+    <div>
+        <h5>[Nom] [Prenom]</h5>
+        <h6> N° de licence : </h6>
+        <h6> Statut : </h6>
+        <span>★★★☆☆</span>
+    </div>
+</div>',
 );
 $joueurs = "";
 foreach ($joueursArray as $joueur) {
-    $joueurs .= $joueur;
+$joueurs .= $joueur;
 }
 
 ?>
@@ -67,46 +67,24 @@ foreach ($joueursArray as $joueur) {
 </head>
 <body>
 
-    <!-- Header -->
-    <header class="main-header">
-        <div class="left-div">
-            <div class="title-container">
-                <h1>🏀<span class="titleText">Handi-Team</span> Manager<span class="titleText"> Pro™</span></h1>
-            </div>
-        </div>
-        <div class="right-div">
-            <a href="page-connexion.php">
-                <span id="compteButton">Compte</span>
-            </a><a href="page-connexion.php">
-                <img class="icon" src="Images/account-avatar.svg" alt="Compte">
-            </a>
-        </div>
-    </header>
+<?php include "barre-navigation.html" ?>
 
-    <!-- Side navigation -->
-    <div class="side-div">
-        <div class="sidenav">
-            <a href="#">Matchs</a>
-            <a href="#">Joueurs</a>
-            <a href="#">Statistiques</a>
 
+    <!-- Page content -->
+    <div class="main">
+        <h2>Vos joueurs</h2>
+
+        <div class="recherche">
+            <input type="text" id="search-bar" name="searchbar" placeholder="Rechercher un joueur...">
+            <button id="search-button">🔍</button>
         </div>
 
-        <!-- Page content -->
-        <div class="main">
-            <h2>Vos joueurs</h2>
-            
-            <div class="recherche">
-                <input type="text" id="search-bar" name="searchbar" placeholder="Rechercher un joueur...">
-                <button id="search-button">🔍</button>
-            </div>
-
-            <div class="flexContainer">
-                <?php echo $joueurs;?>
-            </div>
-            
-
+        <div class="flexContainer">
+            <?php echo $joueurs;?>
         </div>
+
+
     </div>
+</div>
 </body>
 </html>
